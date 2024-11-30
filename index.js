ea770d50ecd454f02ce3a30c3ac60dba787269fb
tngodj <58142798+tngodj@users.noreply.github.com>
2024-11-30 16:53:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const gif = [
 ]
 
 const imageLinks = [
-    'hyunjin0101.github.io/totoro-poster/', // '1.jpg'에 연결된 링크
+    'https://hyunjin0101.github.io/totoro-poster/', // '1.jpg'에 연결된 링크
     'https://hyunjin0101.github.io/cat-poster/', // '7.jpg'에 연결된 링크
     'https://hyunjin0101.github.io/haul-poster/', // '3.jpeg'에 연결된 링크
     'https://hyunjin0101.github.io/sen-poster/', // '4.jpg'에 연결된 링크
